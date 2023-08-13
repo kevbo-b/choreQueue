e24d6d5b87e005e55bc1feed377313a9ef3bd16d
kevbo-b <WhosKev@web.de>
2023-08-13 22:09:25 +0200

    feat: :sparkles: add link to editor (from list)

--- src/app/edit-list/edit-list.component.ts
+++ src/app/edit-list/edit-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-list',
   templateUrl: './edit-list.component.html',
-  styleUrls: ['./edit-list.component.scss']
+  styleUrls: ['./edit-list.component.scss'],
 })
 export class EditListComponent {
+  public constructor(public readonly router: Router) {}
 
+  redirect(path: string): void {
+    this.router.navigate([path]);
+  }
 }
