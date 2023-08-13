2401df3ed626c9325e9b2273df43d36f3eb7f31d
kevbo-b <WhosKev@web.de>
2023-08-13 23:14:40 +0200

    feat: :sparkles: add selection for all tasks

--- src/app/edit-list/edit-list.component.ts
+++ src/app/edit-list/edit-list.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SaveService } from '../services/save.service';
+import { ITask } from '../models/task';
 
 @Component({
   selector: 'app-edit-list',
   templateUrl: './edit-list.component.html',
   styleUrls: ['./edit-list.component.scss'],
 })
-export class EditListComponent {
-  public constructor(public readonly router: Router) {}
+export class EditListComponent implements OnInit {
+  public constructor(
+    public readonly router: Router,
+    public readonly saveService: SaveService
+  ) {}
+
+  tasks: ITask[] = [];
+
+  ngOnInit(): void {
+    this.tasks = this.saveService.getAllTasks();
+  }
 
   redirect(path: string): void {
     this.router.navigate([path]);
